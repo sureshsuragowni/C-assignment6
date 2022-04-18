bd37e5e2cec439c0dc2c14c02d75e9c212a61bed
sureshsuragowni <sureshgowd08@gmail.com>
2022-04-18 09:33:47 +0530

    Comitted

---
+++ BankWithdepo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankingdepwith
+{
+
+    public class BankAccount
+    {
+        public int AccNumber;
+        public string CustomerName;
+        public double Balance;
+
+        public BankAccount(int AccNumber, String CustomerName, double Balance)
+        {
+       
+            this.AccNumber = AccNumber;
+            this.CustomerName = CustomerName;
+            this.Balance = Balance;
+        }
+
+
+        public void Withdraw(double amount)
+        {
+            Console.WriteLine("Enter amount .......");
+            Console.ReadLine();
+            
+
+            if (this.Balance > 500)
+            {
+                if (amount == 100 || amount == 500 || amount == 2000)
+                {
+                    Console.WriteLine(Balance + " : Rs is your Savings,Please choose Account to withdraw");
+                    Console.WriteLine("1.Savings Account");
+                    Console.WriteLine("2.Current Account");
+                    Console.Read();
+                    Console.WriteLine("Your transaction is processing...");
+                    double finalbalance = Balance - amount;
+                    Console.WriteLine(finalbalance + " : here is your balance after debiting the money, If not done by you please inform the local bank officials ");
+                    Console.WriteLine("----------THANK YOU -----------");
+                }
+                else
+                {
+                    Console.WriteLine("transaction has beeen declained");
+                }
+
+            }
+            else if (Balance < 500)
+            {
+                Console.WriteLine("Your balance is less than 500rs .....");
+            }
+            else
+            {
+                Console.WriteLine("Your Session has been Expired!!");
+            }
+
+        }
+        public void Deposit(double amt)
+        {
+            Console.WriteLine("Enter the amount you want to deposit ");
+            Console.Read();
+            if (amt >= 500)
+            {
+                Console.WriteLine("You transaction is under process....");
+                Console.WriteLine("Thank you !! your Transaction is successful");
+                double dep = Balance + amt;
+                Console.WriteLine(dep + ": Rs is your balance after the deposoition");
+                Console.WriteLine("----------THANK YOU FOR AVAILING ATM DEPOSIT SERVICE-----------");
+            }
+            else if (amt < 500)
+            {
+                Console.WriteLine("Please deposit more than 500 Rs");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number!!");
+            }
+        }
+    }
+    internal class WithdrawDeposit
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------WELCOME----------");
+            Console.ReadKey();
+            BankAccount ac1 = new BankAccount(746454, "Suresh Gowd", 746350);
+            ac1.Withdraw(500);
+
+            ;
+        }
+    }
+}
---
+++ EventsBanking.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingDetails
+{
+    public class TransactionEventArgs : EventArgs
+    {
+        public int TranactionAmount { get; set; }
+        public string TranactionType { get; set; }
+
+        public TransactionEventArgs(int amt, string type)
+        {
+            TranactionAmount = amt;
+            TranactionType = type;
+        }
+    }
+    public delegate void TransactionHandler(object sender, TransactionEventArgs e);
+    public class MyEvent
+    {
+        public event TransactionHandler TransactionMade;  
+        public int BalanceAmount;  
+
+        public MyEvent(int amount)  
+        {
+            this.BalanceAmount = amount;
+        }
+
+
+        public void Debit(int debitAmount) 
+        {
+            if (debitAmount < BalanceAmount)
+            {
+                BalanceAmount = BalanceAmount - debitAmount;
+                TransactionEventArgs e = new TransactionEventArgs(debitAmount, "Debited");//event
+                OnTransactionMade(e); 
+            }
+        }
+
+
+
+        public void Credit(int creditAmount)  //credit or deposit method
+        {
+
+            BalanceAmount = BalanceAmount + creditAmount;
+            TransactionEventArgs e = new TransactionEventArgs(creditAmount, "Credited"); //event
+            OnTransactionMade(e); // Credit transaction made
+
+        }
+
+
+
+        public void ZeroBalance()
+        {
+            BalanceAmount = 0;
+            TransactionEventArgs e = new TransactionEventArgs(0, "ZeroBalanceFound"); 
+            OnTransactionMade(e);
+        }
+
+
+
+        protected virtual void OnTransactionMade(TransactionEventArgs e)  
+        {
+            if (TransactionMade != null) 
+            {
+                TransactionMade(this, e); 
+            }
+        }
+    }
+    public class TestMyEvent
+    {
+        private static void SendNotification(Object sender, TransactionEventArgs e) //it should match with delegate returntype
+        {
+            Console.WriteLine("Your Account is {0} for Rs.{1} ", e.TranactionType, e.TranactionAmount);
+
+            //Console.WriteLine("Your Account is UnderBalance",e.TranactionType, e.TranactionAmount);
+        }
+
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("   Mini Statement  ");
+            MyEvent ac2 = new MyEvent(200);
+
+            if (ac2.BalanceAmount > 500)
+            {
+                ac2.TransactionMade += new TransactionHandler(SendNotification);
+            }
+            else if (ac2.BalanceAmount < 500 && ac2.BalanceAmount > 0)
+            {
+        
+                Console.WriteLine("Your account is UnderBalance");
+            }
+            else if (ac2.BalanceAmount == 0)
+            {
+                ac2.ZeroBalance();
+                ac2.TransactionMade += new TransactionHandler(SendNotification);
+            }
+            
+            ac2.Credit(2000);
+            Console.WriteLine("Your Current Balance is : " + ac2.BalanceAmount);
+            Console.ReadLine();
+            Console.WriteLine("* Rs 999 is Auto debited as Maintainance charge by Your Bank");
+            Console.WriteLine("Press enter to view Total balance");
+            Console.ReadKey();
+            ac2.Debit(500);
+            Console.WriteLine("Your Current Balance is : " + ac2.BalanceAmount);
+            Console.ReadLine();
+        }
+    }
+}
